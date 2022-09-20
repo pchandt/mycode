cd159e9b8835cc76fe38129ed0b491f4591f8eb2
Pratibha <pratibha@users.noreply.github.com>
2022-09-20 17:59:44 +0000

    mixed lists updated

--- mix-list/mixlist01.py
+++ mix-list/mixlist01.py
@@ -14,5 +14,8 @@ print("The last item in the list (state): " + my_list[2] )
 #create another list
 iplist = [ 5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh" ]
 
-#display only the IP addresses
+#display only the IP addresses from iplist
 print(f"IP addresses: {iplist[3]}, and {iplist[4]}")
+
+#another way of printing only IP addresses
+print("IP addresses: " + iplist[3] + ", and " + iplist[4]) 
