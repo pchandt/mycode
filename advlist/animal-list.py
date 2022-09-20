00e2a9892e2515aa533b6b8ef1949265a0aa7dd0
Pratibha <pratibha@users.noreply.github.com>
2022-09-20 20:22:43 +0000

    About complex lists

---
+++ advlist/animal-list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Alta3 Research | RZFeeser
+   List - animal list"""
+
+def main():
+    # create a list of animal names called list1
+    list1 = ["Cat", "Dog", "Cow", "Fox", "Bat"]
+
+    # display all the elements of list1
+    print(*list1)
+
+main()
