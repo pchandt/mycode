836ce936f9256a0671d33c2e2776932908df0f1b
Pratibha <pratibha@users.noreply.github.com>
2022-09-20 02:22:25 +0000

    input function challenge01 revision

--- challenges/challenge01.py
+++ challenges/challenge01.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python3
+"""Alta3 Research | RZFeeser
+     print() - display data to std out"""
+
 # below is a function containing our code
 def main():
-   
-    # input for user_name
-    name = input("Please enter your name:")
 
-    # input for the day
-    week_day = input("What day of the week is today?")
+    # pause the program and wait for the user to provide input
+    user_name = input("Please enter your name!")
 
-    # A print statement that display the input back to the user.
-    print("Hello, " + name + "! Happy " + week_day + "!")
+    # asking user for day of the week
+    week_day = input("What day of the week is it?")
 
+    # display the input back to the user
+    print(f"Hello, {user_name}! Happy {week_day}!")
 
 # this calls main
-    main()
+main()
 
