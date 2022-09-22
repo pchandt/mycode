c0f38ae55f58af2fc674c90fc581b05ade50a7a2
Pratibha <pchandt@users.noreply.github.com>
2022-09-22 17:20:47 +0000

    Learning to troubleshoot

--- broken01/ifixed.py
+++ broken01/ifixed.py
@@ -7,19 +7,19 @@ calc2 = 0.0
 operation = ""
 while (calc1 != "q"):
     print("\nWhat is the first operator? Or, enter q to quit: ")
-    calc1 = raw_input()
-    if calc1 == "Q":
+    calc1 = input()
+    if calc1 == "q":
         break
     calc1 = float(calc1)
     print("\nWhat is the second operator? Or, enter q to quit: ")
-    calc2 = raw_input()
+    calc2 = input()
     if calc2 == "q":
         break
     calc2 = float(calc2)
     print("Enter an operation to perform on the two operators (+ or -): ")
-    operation = raw_input()
+    operation = input()
     if operation == "+":
-        print(\n + str(calc1) + " + " + str(calc2) + " = " + str(calc1 + calc2))
+        print("\n" + str(calc1) + " + " + str(calc2) + " = " + str(calc1 + calc2))
     elif operation == '-':
         print("\n" + str(calc1) + " - " + str(calc2) + " = " + str(calc1 - calc2))
     else:
