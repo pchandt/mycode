3efcdfdd45361794c73d77515e7c7ee7aced0e48
pchandt <pcthakuri26@gmail.com>
2022-09-28 21:28:41 +0000

    Jinja templating for HTML and configs

--- mtg/api-challenge.py
+++ mtg/api-challenge.py
@@ -2,6 +2,7 @@
 """Returning the location of the ISS in latitude/longitude"""
 import requests
 import datetime
+import reverse_geocoder as rg
     
 URL= "http://api.open-notify.org/iss-now.json"
 def main():
@@ -14,7 +15,11 @@ def main():
     timestamp = resp["timestamp"]
     timestamp = datetime.datetime.fromtimestamp(timestamp)
 
-    print(f"CURRENT LOCATION OF THE ISS:\nTimestamp: {timestamp}\nLon = {longitude}\nLat = {latitude}")
+    locator_resp = rg.search(latitude, longitude)
+
+    city = locator_resp[0]["cc"]
+
+    print(f"CURRENT LOCATION OF THE ISS:\nTimestamp: {timestamp}\nLon = {longitude}\nLat = {latitude}\nCity/Country : {city}, {country}")
 
 if __name__ == "__main__":
     main()
